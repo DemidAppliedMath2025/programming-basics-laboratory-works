fdd286d4586824dad98562a0ce337e3bb62cf1c7
Fenek <velikayasoplya@gmail.com>
2025-09-26 15:22:20 +0300

    Update variant_6.cpp

--- Semester-1/laboratory_work_1/task_1/variant_6.cpp
+++ Semester-1/laboratory_work_1/task_1/variant_6.cpp
@@ -1,3 +1,7 @@
+#include <cmath>
+#include <iostream>
+#include <iomanip>
+
 double expression_a(double p, double q) {
     double denominator = pow((sqrt(pow(p, 3)) / sqrt(p) + p), 0.25) / pow(pow((p - q), 3), 0.125);
     double divisor = pow(sqrt(p) / (sqrt(p) - sqrt(q)) - sqrt(p / q) + 1, 0.25);
