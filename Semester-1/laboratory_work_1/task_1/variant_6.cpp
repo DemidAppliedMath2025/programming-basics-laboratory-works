140c63bb9ab706f70114d1a5b16b3b783ec432b6
Fenek <velikayasoplya@gmail.com>
2025-09-26 15:09:30 +0300

    Create variant_6.cpp

---
+++ Semester-1/laboratory_work_1/task_1/variant_6.cpp
@@ -0,0 +1,31 @@
+double expression_a(double p, double q) {
+    double denominator = pow((sqrt(pow(p, 3)) / sqrt(p) + p), 0.25) / pow(pow((p - q), 3), 0.125);
+    double divisor = pow(sqrt(p) / (sqrt(p) - sqrt(q)) - sqrt(p / q) + 1, 0.25);
+
+    return denominator / divisor;
+}
+
+double expression_b(double p, double q) {
+    return 1 / (pow(p - q, 0.125));
+}
+
+int main() {
+    std::string str_p, str_q;
+
+    std::cin >> str_p >> str_q;
+
+    try {
+        double p = std::stod(str_p);
+        double q = std::stod(str_q);
+
+        if (!(p != q && p >= 0 && q > 0 && sqrt(p) / (sqrt(p) - sqrt(q)) - sqrt(p / q) + 1 > 0)) {
+            std::cout << "Numbers don't fit in range";
+        } else {
+            std::cout << std::fixed << std::setprecision(6) << expression_a(p, q) - expression_b(p, q);
+        }
+    } catch (...) {
+        std::cout << "Some argument(s) is(are) not number(s)";
+
+        return;
+    }
+}
