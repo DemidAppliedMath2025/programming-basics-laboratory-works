e9b70c214917605dd90fe74be1ad64d988dd7d03
Fenek <velikayasoplya@gmail.com>
2025-09-26 15:17:31 +0300

    Create variant_16.cpp

---
+++ Semester-1/laboratory_work_1/task_5/variant_16.cpp
@@ -0,0 +1,19 @@
+bool expression_a(int x, int y, int z) {
+    return !(!(!x * y + x * !y) * y) + x * z;
+}
+
+bool expression_b(int x, int y, int z) {
+    return !(x + z * (x + !z)) + (!x * y + x * !y);
+}
+
+int main() {
+    std::cout << "x y z A B" << "\n";
+
+    for (int x = 0; x < 2; x ++) {
+        for (int y = 0; y < 2; y++) {
+            for (int z = 0; z < 2; z++) {
+                std::cout << x << " " << y << " " << z << " " << expression_a(x, y, z) << " " << expression_b(x, y, z) << "\n";
+            }
+        }
+    }
+}
