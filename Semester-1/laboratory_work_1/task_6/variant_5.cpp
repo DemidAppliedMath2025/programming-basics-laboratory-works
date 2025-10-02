aa99cc6b597cfb6fdbc4f2ef74c776fcb25dc2a0
Fenek <velikayasoplya@gmail.com>
2025-10-02 14:14:06 +0300

    Update variant_5.cpp

--- Semester-1/laboratory_work_1/task_6/variant_5.cpp
+++ Semester-1/laboratory_work_1/task_6/variant_5.cpp
@@ -2,48 +2,38 @@
 #include <iostream>
 
 int main() {
-    std::string n_str;
+    int n;
 
     std::cout << "Enter the amount of numbers you want to input" << "\n";
-    std::cin >> n_str;
+    std::cin >> n;
 
-    try {
-        int n = std::stoi(n_str);
-        int input;
-        int numbers[n];
-        std::string output;
+    int input;
+    int numbers[n];
+    std::string output;
 
-        std::cout << "Now enter the numbers themselves" << "\n";
+    std::cout << "Now enter the numbers themselves" << "\n";
+    std::cin >> input;
+
+    numbers[0] = input;
+
+    for (int i = 1; i < n; ++i) {
         std::cin >> input;
 
-        while (!input) {
+        if (!input) {
             std::cout << "The input is not a natural number";
-            std::cin >> input;
         }
 
-        numbers[0] = input;
+        numbers[i] = input;
 
-        for (int i = 1; i < n; i++) {
-            std::cin >> input;
+        double x1 = -sqrt(numbers[i - 1] - numbers[i]);
+        double x2 = -sqrt(numbers[i - 1] - numbers[i]);
 
-            if (!input) {
-                std::cout << "The input is not a natural number";
-            }
+        !(((static_cast<int>(x1) >> 31) & 1) && ((static_cast<int>(x2) >> 31) & 1)) ? output += std::to_string(input) : output += "";
+    }
 
-            numbers[i] = input;
-
-            double x1 = -sqrt(numbers[i] - numbers[i - 1]);
-            double x2 = -sqrt(numbers[i] - numbers[i - 1]);
-
-            !(((static_cast<int>(x1) >> 31) & 1) && ((static_cast<int>(x2) >> 31) & 1)) ? output += std::to_string(input) : output += "";
-        }
-
-        if (output.empty()) {
-            std::cout << "No such elements that fit the condition";
-        } else {
-            std::cout << output;
-        }
-    } catch (...) {
-        std::cout << "Argument is not an integer";
+    if (output.empty()) {
+        std::cout << "No such elements that fit the condition";
+    } else {
+        std::cout << output;
     }
 }
