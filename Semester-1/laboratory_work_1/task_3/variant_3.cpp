6682dae960dfef8c1ce524bf7034e1826d6cbf46
Fenek <velikayasoplya@gmail.com>
2025-10-02 14:04:30 +0300

    Update variant_3.cpp

--- Semester-1/laboratory_work_1/task_3/variant_3.cpp
+++ Semester-1/laboratory_work_1/task_3/variant_3.cpp
@@ -3,14 +3,14 @@
 int main() {
     // "ИКС+ИСК=КСИ";
 
-    for (int i = 0; i < 10; i++) {
-        for (int k = 0; k < 10; k++) {
-            for (int s = 0; s < 10; s++) {
+    for (int i = 0; i < 10; ++i) {
+        for (int k = 0; k < 10; ++k) {
+            for (int s = 0; s < 10; ++s) {
                 int first = i * 100 + k * 10 + s;
                 int second = i * 100 + s * 10 + k;
                 int third = k * 100 + s * 10 + i;
 
-                if (first + second == third) {
+                if (first + second == third && first != second != third) {
                     std::cout << "I: " << i << "\n";
                     std::cout << "K: " << k << "\n";
                     std::cout << "S: " << s << "\n";
