78e58d9ab6077eb2037d0c95aff9973fd2c06f5f
Fenek <velikayasoplya@gmail.com>
2025-10-15 20:28:13 +0300

    Create variant_14.cpp

---
+++ Semester-2/laboratory_work_x/task_1/variant_14.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <algorithm>
+
+int sort_alg(int x, int y) {
+    return x >= 0 && y < 0;
+}
+
+int main() {
+    int n;
+    std::cin >> n;
+
+    int* array = new int[n];
+
+    for (int i = 0; i < n; ++i) {
+        std::cin >> array[i];
+    }
+
+    std::sort(array, array + n, sort_alg);
+
+    for (int i = 0; i < n; i++) {
+        std::cout << array[i] << " ";
+    }
+
+    delete[] array;
+
+    return 0;
+}
