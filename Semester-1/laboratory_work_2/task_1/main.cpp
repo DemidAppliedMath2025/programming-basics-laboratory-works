24775103bac141f600e2929f4f9375a1951a8fe3
Fenek <velikayasoplya@gmail.com>
2025-10-30 18:11:23 +0300

    Update main.cpp

--- Semester-1/laboratory_work_2/task_1/main.cpp
+++ Semester-1/laboratory_work_2/task_1/main.cpp
@@ -1,1 +1,25 @@
+#include <iostream>
+#include <algorithm>
 
+int sort_alg(int x, int y) {
+    return x >= 0 && y < 0;
+}
+
+int main() {
+    int n;
+    std::cin >> n;
+
+    int array[100];
+
+    for (int i = 0; i < n; ++i) {
+        std::cin >> array[i];
+    }
+
+    std::sort(array, array + n, sort_alg);
+
+    for (int i = 0; i < n; ++i) {
+        std::cout << array[i] << " ";
+    }
+
+    return 0;
+}
